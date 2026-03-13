1ebdf6388ad228c356f7c8af3b524fe6daef0a01
Edgar-PTC <20230280@ricaldone.edu.sv>
2026-03-13 11:22:53 -0600

    Subiendo cambios, CRUD entero 2

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import app from './app.js';
 import "./database.js";
+import { config } from "./src/config.js";
 
 //creamos la funcion que se encargara de iniciar el servidor
 async function main() {
