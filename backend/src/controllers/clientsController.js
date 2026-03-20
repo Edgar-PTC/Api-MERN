90c8c1927a7176aeb803e04e258fa298ad600abd
Edgar-PTC <20230280@ricaldone.edu.sv>
2026-03-20 11:24:15 -0600

    Agregando clients y admins

--- backend/src/controllers/clientsController.js
+++ backend/src/controllers/clientsController.js
@@ -24,7 +24,9 @@ clientsController.insertClients = async (req, res) => {
             return res.status(400).json({message: "Todos los campos son requeridos"})
         }
 
-        if(birthday >== Date.now())
+        if(birthday >= Date.now()){
+            return res.status(400).json({message: "la fecha no puede ser hoy o una en un futuro"})
+        }
 
         if(name.lenght < 3){
             return res.status(400).json({message: "name too short"})
@@ -67,7 +69,37 @@ clientsController.deleteClients = async (req, res) => {
 
 clientsController.updateClients = async (req, res) => {
     try {
+        let {name, email, password, birthday, status, isVerified, loginAttemps, timeOut} = req.body;
         
+        name = name?.trim();
+        email = email?.trim();
+        password = password?.trim();
+
+        if(!name || !email || !password){
+            return res.status(400).json({message: "Todos los campos son requeridos"})
+        }
+
+        if(birthday >= Date.now()){
+            return res.status(400).json({message: "la fecha no puede ser hoy o una en un futuro"})
+        }
+
+        if(name.lenght < 3){
+            return res.status(400).json({message: "name too short"})
+        }
+
+        //Validacion de email ReGex
+        const emailRegex = /^[^\s@]+@[^\s]@+\.[^\s@]+$/
+        if (!emailRegex.test(email)){
+            return res.status(400).json({message: "Invalid email format"})
+        }
+
+        if(password.lenght < 5){
+            return res.status(400).json({message: "The password must be at least 5 characters long."})
+        }
+
+        const updateClient = await clientsModel.findByIdAndUpdate(req.params.id, {name, email, password, birthday, status, isVerified, loginAttemps, timeOut}, {new: true})
+
+        return res.status(200).json({message: "Client modified"})
     } catch (error) {
         console.log("Error: " + error);
         return res.status(500).json({message: "Internal server error"});
