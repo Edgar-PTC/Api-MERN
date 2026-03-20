4b76f457e965879a75de09dd38a3b81623254aaa
Edgar-PTC <20230280@ricaldone.edu.sv>
2026-03-20 11:20:43 -0600

    Agregando clients y admins

--- backend/src/controllers/adminsController.js
+++ backend/src/controllers/adminsController.js
@@ -4,7 +4,8 @@ import adminsModel from "../models/admins.js"
 
 adminsController.getAdmins = async (req, res) => {
     try {
-        
+        const admins = await adminsModel.find();
+        return res.status(200).json(admins);
     } catch (error) {
         console.log("Error: " + error);
         return res.status(500).json({message: "Internal server error"});
@@ -13,7 +14,34 @@ adminsController.getAdmins = async (req, res) => {
 
 adminsController.insertAdmins = async (req, res) => {
     try {
-        
+        let {name, email, password, isVerified} = req.body;
+
+        name = name?.trim();
+        email = emial?.trim();
+        password = password?.();
+
+        if(!name || !email || !password){
+            return res.status(400).json({message: "Todos los campos son requeridos"})
+        }
+
+        if(name.lenght < 3){
+            return res.status(400).json({message: "name too short"})
+        }
+
+        //Validacion de email ReGex
+        const emailRegex = /^[^\s@]+@[^\s]@+\.[^\s@]+$/
+        if (!emailRegex.test(email)){
+            return res.status(400).json({message: "Invalid email format"})
+        }
+
+        if(password.lenght < 5){
+            return res.status(400).json({message: "The password must be at least 5 characters long."})
+        }
+
+        const newAdmin = adminsModel({name, email, password, isVerified})
+        await newAdmin.save();
+
+        return res.status(201).json({message: "Admin saved"})
     } catch (error) {
         console.log("Error: " + error);
         return res.status(500).json({message: "Internal server error"});
@@ -22,7 +50,13 @@ adminsController.insertAdmins = async (req, res) => {
 
 adminsController.deleteAdmins = async (req, res) => {
     try {
-        
+        const deleteAdmin = await adminsModel.findByIdAndDelete(req.params.id)
+
+        if(!deleteAdmin){
+            return res.status(400).json({message: "Admin not found"})
+        }
+
+        return res.status(200).json({message: "Admin deleted"})
     } catch (error) {
         console.log("Error: " + error);
         return res.status(500).json({message: "Internal server error"});
@@ -31,7 +65,29 @@ adminsController.deleteAdmins = async (req, res) => {
 
 adminsController.updateAdmins = async (req, res) => {
     try {
-        
+        let {name, email, password, isVerified} = req.body;
+
+        name = name?.trim();
+        email = emial?.trim();
+        password = password?.();
+
+        if(name.lenght < 3){
+            return res.status(400).json({message: "name too short"})
+        }
+
+        //Validacion de email ReGex
+        const emailRegex = /^[^\s@]+@[^\s]@+\.[^\s@]+$/
+        if (!emailRegex.test(email)){
+            return res.status(400).json({message: "Invalid email format"})
+        }
+
+        if(password.lenght < 5){
+            return res.status(400).json({message: "The password must be at least 5 characters long."})
+        }
+
+        const updated = await adminsModel.findByIdAndUpdate(req.params.id, {name, email, password, isVerified}, {new: true})
+
+        return res.status(200).json({message: "Admin modified"})
     } catch (error) {
         console.log("Error: " + error);
         return res.status(500).json({message: "Internal server error"});
--- backend/src/controllers/clientsController.js
+++ backend/src/controllers/clientsController.js
@@ -4,7 +4,8 @@ import clientsModel from "../models/clients.js";
 
 clientsController.getClients = async (req, res) => {
     try {
-        
+        const clients = await clientsModel.find();
+        return res.status(200).json(clients);
     } catch (error) {
         console.log("Error: " + error);
         return res.status(500).json({message: "Internal server error"});
@@ -13,7 +14,36 @@ clientsController.getClients = async (req, res) => {
 
 clientsController.insertClients = async (req, res) => {
     try {
+        let {name, email, password, birthday, status, isVerified, loginAttemps, timeOut} = req.body;
         
+        name = name?.trim();
+        email = email?.trim();
+        password = password?.trim();
+
+        if(!name || !email || !password){
+            return res.status(400).json({message: "Todos los campos son requeridos"})
+        }
+
+        if(birthday >== Date.now())
+
+        if(name.lenght < 3){
+            return res.status(400).json({message: "name too short"})
+        }
+
+        //Validacion de email ReGex
+        const emailRegex = /^[^\s@]+@[^\s]@+\.[^\s@]+$/
+        if (!emailRegex.test(email)){
+            return res.status(400).json({message: "Invalid email format"})
+        }
+
+        if(password.lenght < 5){
+            return res.status(400).json({message: "The password must be at least 5 characters long."})
+        }
+
+        const newClient = clientsModel({name, email, password, birthday, status, isVerified, loginAttemps, timeOut})
+        await newClient.save()
+
+        return res.status(201).json({message: "Client saved"})
     } catch (error) {
         console.log("Error: " + error);
         return res.status(500).json({message: "Internal server error"});
@@ -22,7 +52,13 @@ clientsController.insertClients = async (req, res) => {
 
 clientsController.deleteClients = async (req, res) => {
     try {
+        const deleteClient = await clientsModel.findByIdAndDelete(req.params.id)
+
+        if(!deleteClient){
+            return res.status(400).json({message: "Client not found"})
+        }
         
+        return res.status(200).json({message: "Client deleted"})
     } catch (error) {
         console.log("Error: " + error);
         return res.status(500).json({message: "Internal server error"});
